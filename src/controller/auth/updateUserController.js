dcc98447c91f88d0b1aab4066509fbb21f9d4db5
thatAwesomeKK <2550492.kk@gmail.com>
2023-02-19 20:50:52 +0530

    Fixed No Username Bug
    Fixed username not coming Bug on Liked Rings and Downloaded Rings API

--- src/controller/auth/updateUserController.js
+++ src/controller/auth/updateUserController.js
@@ -22,7 +22,6 @@ export default async function (req, res) {
 
         return res.status(200).json({ success: true, message: "User Updated SuccessFully!" });
     } catch (error) {
-        res.clearCookie('refreshToken', cookieConfig);
-        return res.status(500).json({ success: false, error: error })
+        return res.status(500).json({ success: false, error: "Internal Server Error!" })
     }
 }
--- src/controller/user/getLikedRingsController.js
+++ src/controller/user/getLikedRingsController.js
@@ -7,7 +7,7 @@ export default async function (req, res) {
         if (!user) {
             return res.status(400).send({ error: "User Does Not Exist" })
         }
-        const rings = await Ringtone.find({ likes: { "$in": [user.id] } }).select('ringID title thumbnail likes origin downloads createdAt')
+        const rings = await Ringtone.find({ likes: { "$in": [user.id] } }).populate({ path: 'uid', model: User, select: 'username' }).select('ringID title thumbnail likes origin downloads createdAt')
         if (!rings) {
             return res.status(400).send({ error: "Rings Does Not Exist" })
         }
